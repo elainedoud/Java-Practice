907f5f946f3b6fd02b224ec313b19ca16f826f59
Elaine <elainedoud@gmail.com>
2023-11-02 15:28:20 -0400

    Implemented Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+
+// To compile a Java file, do javac file name
+// Then to run the file, do java file name
+// Example:  javac HelloWorld.java
+//   java HelloWorld.java
+// (The file and class must have matching names or it will not work)
